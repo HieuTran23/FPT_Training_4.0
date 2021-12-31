a18f9867d3c51e5b754eb292651491ec68151ef2
Hieu <tdhieu23@gmail.com>
2021-12-31 19:00:40 +0700

    Account function ((View)User Role + (Add+ view)Account)

---
+++ FPT_Training_4.0/Controllers/ManageUserController.cs
@@ -0,0 +1,47 @@
+﻿using FPT_Training_4._0.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FPT_Training_4._0.Controllers
+{
+    public class ManageUserController : Controller
+    {
+        ApplicationDbContext context;
+        public ManageUserController()
+        {
+            context = new ApplicationDbContext();
+        }
+
+        // GET: ManageUser
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult UsersWithRoles()
+        {
+            var usersWithRoles = (from user in context.Users
+                                  select new
+                                  {
+                                      UserId = user.Id,
+                                      Username = user.UserName,
+                                      Email = user.Email,
+                                      RoleNames = (from userRole in user.Roles
+                                                   join role in context.Roles on userRole.RoleId
+                                                   equals role.Id
+                                                   select role.Name).ToList()
+                                  }).ToList().Select(p => new UserRoleViewModel()
+
+                                  {
+                                      UserId = p.UserId,
+                                      Username = p.Username,
+                                      Email = p.Email,
+                                      Role = string.Join(",", p.RoleNames)
+                                  });
+            return View(usersWithRoles);
+        }
+    }
+}
---
+++ FPT_Training_4.0/Controllers/UsersController.cs
@@ -0,0 +1,59 @@
+﻿using FPT_Training_4._0.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FPT_Training_4._0.Controllers
+{
+    public class UsersController : Controller
+    {
+        // GET: Users
+        public ActionResult Index()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                var user = User.Identity;
+                ViewBag.Name = user.Name;
+
+                ViewBag.displayMenu = "No";
+
+                if (isAdminUser())
+                {
+                    ViewBag.displayMenu = "Yes";
+                }
+                return View();
+            }
+            else
+            {
+                ViewBag.Name = "Not Logged IN";
+            }
+            return View();
+
+        }
+
+        //Check admin user
+        public Boolean isAdminUser()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                var user = User.Identity;
+                ApplicationDbContext context = new ApplicationDbContext();
+                var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+                var s = UserManager.GetRoles(user.GetUserId());
+                if (s[0].ToString() == "Admin")
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return false;
+        }
+    }
+}
