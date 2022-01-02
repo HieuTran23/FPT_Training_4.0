448eacd96548c521f782ce472040ccf189cf4d8f
Hieu <tdhieu23@gmail.com>
2022-01-02 13:39:36 +0700

    Test commit

---
+++ FPT_Training_4.0/Models/Course.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FPT_Training_4._0.Models
+{
+    public class Course
+    {
+        public Course()
+        {
+            CreateDate = DateTime.Now;
+        }
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string TypeCourse { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public DateTime CreateDate { get; set; }
+    }
+}
